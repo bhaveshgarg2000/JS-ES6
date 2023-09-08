42e34a29ed9f45d31b4094af179e311a2c299d8c
BHAVESH KRISHAN GARG <72064250+bhaveshgarg2000@users.noreply.github.com>
2023-09-08 10:24:11 +0000

    COMPARISON

---
+++ JS/Variables/comparison.js
@@ -0,0 +1,11 @@
+// console.log(2>1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.table([null>0,null<0,null<=0,null>=0,null<=0])
+
+// === : Strict Check value and type check of values
+
+console.table(["1"===1,1 ===1,true === true,'a'==='b'])
