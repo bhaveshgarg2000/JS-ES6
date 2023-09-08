74741f577a4eeb3a961a1cb4364056c90aaedd9e
BHAVESH KRISHAN GARG <72064250+bhaveshgarg2000@users.noreply.github.com>
2023-09-08 08:14:59 +0000

    dataTypes

---
+++ JS/Variables/2.js
@@ -0,0 +1,36 @@
+"use strict" // treat all JS code to newer version
+
+// alert("Hello") // we are using node not browser
+
+
+console.log(3+3)
+console.log("Hites")
+
+
+let name = "Hitesh";
+let age = 25;
+let isLoggesIn = true
+let state = null
+
+/*
+
+number = 2 to the power 53
+bigInt
+string = double and single quotes
+boolean = true and false
+NULLL = standalone value
+undefined = value not defined yet
+symbol : unique
+
+
+
+object
+*/
+
+console.table([typeof(name),typeof(age),typeof(isLoggesIn),typeof(state)])
+console.table([typeof undefined,typeof null]) 
+// console.table([typeof NULL]) 
+/*
+typeof (undefined) : Undefined
+typeof null = object
+*/
---
+++ JS/Variables/let.js
@@ -0,0 +1,22 @@
+const accId = 555;
+let accntEmail = "hitesh@gmail.com"
+var accountPass = "123454"
+accountCity = "Jaipur"
+let accountSate 
+// accId = 3
+// console.log(accId)
+accntEmail = "pepsi@gmail.com"
+// console.log(accntEmail)
+accountPass = "555"
+// console.log(accId)
+
+accountCity = "Bengaluru"
+console.table([accId,accntEmail,accountPass,accountCity,accountSate])
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+
+*/ 
