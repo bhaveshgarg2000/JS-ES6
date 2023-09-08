8d37127a348eafc4131956017a60cfde2d478007
BHAVESH KRISHAN GARG <72064250+bhaveshgarg2000@users.noreply.github.com>
2023-09-08 08:44:23 +0000

    Operations

---
+++ JS/Variables/operations.js
@@ -0,0 +1,16 @@
+let value = 3
+let negValue = -value
+console.log(value,negValue)
+console.log(2+2);
+// console.table([2+2,2-2,2+ +"2"])
+// console.table([2 + - "2",2  * +"8",2**2,2/3,3%3])
+let str1 = "Bhavesh"
+let str2 = "Hello ";
+// console.log(str2+ str1)
+// String Concatination
+
+console.table([ 2+"1",1+"2" + 3, 1+1+2+ +"3",1+2+"3",3+4+5*3%3])
+
+
+
+
