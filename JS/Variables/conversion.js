20055337e96ff1768233e36e4f3b24d99577eda9
BHAVESH KRISHAN GARG <72064250+bhaveshgarg2000@users.noreply.github.com>
2023-09-08 08:28:30 +0000

    Conversions

---
+++ JS/Variables/conversion.js
@@ -0,0 +1,35 @@
+let score = true;
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)
+console.log(typeof NaN)
+
+
+/*  
+
+
+"33" = 33
+"33aaa" = NaN
+
+true = 1
+false = 0;
+"" = false
+"bh"  : true
+*/
+
+
+let isLoggedIn = "Bh";
+let boolCheck = Boolean(isLoggedIn)
+// console.table([isLoggedIn,boolCheck])
+// console.table([typeof isLoggedIn,typeof boolCheck])
+
+let someNum = 33
+let st = String(someNum)
+console.log( st)
+console.table([typeof someNum,typeof st])
+
+
+
+
