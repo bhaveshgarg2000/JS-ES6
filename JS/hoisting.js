1a414a3d46d5bd18869319c7ba76cfce06007a2c
BHAVESH KRISHAN GARG <72064250+bhaveshgarg2000@users.noreply.github.com>
2023-09-12 15:46:36 +0000

    Hoisting in JS

---
+++ JS/hoisting.js
@@ -0,0 +1,27 @@
+// console.log(fun);
+// fun()
+// sum()
+// console.log(sum)
+// sum
+
+
+
+get();
+console.log(get)
+var x = 7;
+
+function fun() {
+  console.log("Hiii");
+}
+// now it will behave like variable
+let sum = () => {
+  console.log(`Bye`);
+};
+let get = function(){
+    console.log("Log");
+}
+
+
+let name 
+
+console.log(`x : ${x}`);
